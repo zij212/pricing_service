b5ca85a0721581cd04336f536e508bc2adb26020
Zinan Jiang <zinan.jiang@marriott.com>
2020-03-09 00:13:29 -0400

    add MONGOLAB_URI

--- common/database.py
+++ common/database.py
@@ -1,9 +1,11 @@
+import os
+
 import pymongo
 from typing import Dict
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017/pricing"
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = pymongo.MongoClient(URI).get_database()
 
     @staticmethod
